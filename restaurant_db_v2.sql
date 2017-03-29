1f3ba62327d883705d597c0a8e96c964774e5bd2
Ning Yuan <nfyuan@gmail.com>
2017-03-28 21:40:53 -0400

    exercises on associations and aggregation

---
+++ restaurant_db_v2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE restaurant(
+  id serial PRIMARY KEY,
+  name varchar,
+  address varchar,
+  category varchar
+
+);
+
+CREATE TABLE reviewer(
+  id serial PRIMARY KEY,
+  name varchar,
+  email varchar,
+  karma integer CHECK (karma >=0 and karma <=7) 
+
+);
+
+
+CREATE TABLE review(
+  id serial PRIMARY KEY,
+  reviewer_id integer REFERENCES reviewer (id),
+  stars integer CHECK (stars >=0 and stars <=5),
+  title varchar,
+  review varchar,
+  restaurant_id integer REFERENCES restaurant (id)
+);
